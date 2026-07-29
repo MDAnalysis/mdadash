b6cf0d2748368822dbd51bdc4bef3b75286f0d2f
Pardhav Maradani <pardhav.maradani@gmail.com>
2026-07-29 16:24:57 +0530

    Update custom code widget doc

--- mdadash/backend/analyses/custom_code.py
+++ mdadash/backend/analyses/custom_code.py
@@ -11,7 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 class CustomCode(WidgetBase):
-    """Custom Code
+    """
+
+    **Custom Code**
 
     This widget allows custom user-defined code to run during widget execution.
     The run frequency can be `every-frame` or `batch` similar to other widgets.
@@ -22,40 +24,82 @@ class CustomCode(WidgetBase):
         :alt: Custom code widget inputs
 
     The code under **Setup code** is executed only once at the time of widget creation.
-    Examples of code that goes here would be common functions, initialization blocks etc.
+    Examples of code that goes here would be common functions, class definitions,
+    initializations etc.
 
     The code under **Execute code** is executed as per the chosen run frequency. The
-    outputs of this code is what will show up as the widget outputs. The outputs can
-    include both text and images. Any errors, including syntax errors and code errors
-    will show up in the output as well.
+    outputs of this code will show up as the widget outputs. The outputs can include
+    both text and images. Any errors, including syntax errors and code errors will show
+    up in the output as well.
 
-    The variable ``u`` points to the current MDAnalysis ``Universe`` and can be used
-    directly in the code. There is support for code complete and inspect as shown below:
+    The global variable ``u`` points to the current MDAnalysis ``Universe`` and can be
+    used directly in the code. There is support for code complete and inspect as shown
+    below:
 
     .. figure:: /_static/images/custom-code-widget-code-complete-inspect.png
         :alt: Code complete and inspect
 
     Examples
     --------
+
+    **Example 1: Box volume**
+
     To print the current box volume, the following can be used in the **Execute code**:
 
-    >>> print(f"Box Volume = {u.trajectory.ts.volume:.2f} Å³")
+    .. code-block:: python
 
-    To print / display the Kinetic Energy of an ``AtomGroup`` a simple KE function can
-    be defined in the **Setup code** as follows (executes only once):
+        print(f"Box Volume = {u.trajectory.ts.volume:.2f} Å³")
 
-    >>> import numpy as np
-    >>> def ke(ag):
-    >>>     return 0.5 * np.sum(ag.masses[:, np.newaxis] * (ag.velocities**2))
+    **Example 2: Kinetic Energy of an AtomGroup**
 
-    The above function can be invoked in the **Execute code** section as follows:
+    To print and display the Kinetic Energy of an ``AtomGroup`` a simple KE function
+    and plot can be defined in the **Setup code** as follows (executes only once):
 
-    >>> ke1 = ke(u.select_atoms("protein"))
-    >>> print(f"Protein KE = {ke1:.2f}")
-    >>> plt1.show("KE (protein)", ke1)
+    .. code-block:: python
 
-    The **Setup code** can also include code to create ``matplotlib`` plots that can be
-    displayed in the **Execute code** section.
+        import numpy as np
+        from collections import deque
+        import matplotlib.pyplot as plt
+
+        def ke(ag):
+            return 0.5 * np.sum(ag.masses[:, np.newaxis] * (ag.velocities**2))
+
+        class SimplePlot:
+            def __init__(self, universe, max_values=100):
+                self.u = universe
+                self.times = deque(maxlen=max_values)
+                self.values = deque(maxlen=max_values)
+
+            def show(self, name, values):
+                self.values.append(values)
+                self.times.append(self.u.trajectory.ts.data["time"])
+                plt.plot(self.times, self.values)
+                plt.ylabel("Values")
+                plt.xlabel("Time (ps)")
+                plt.title(name)
+                plt.grid(True)
+                plt.show()
+
+        plt1 = SimplePlot(u)
+
+    The above function and plot can be invoked in the **Execute code** section as
+    follows:
+
+    .. code-block:: python
+
+        ke1 = ke(u.select_atoms("protein"))
+        print(f"Protein KE = {ke1:.2f}")
+        plt1.show("KE (protein)", ke1)
+
+    Here is an example of the widget output when the widget is running:
+
+    .. figure:: /_static/images/custom-code-widget-output.png
+        :alt: Widget output
+
+    .. note::
+
+        Variables created in custom code blocks are common across the dashboard
+        kernel and hence must be chosen to not overwrite each other.
 
     """
 
