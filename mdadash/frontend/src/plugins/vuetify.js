8ee1144cd07dca67b227d6c8543d9c1ccfb3000a
Pardhav Maradani <pardhav.maradani@gmail.com>
2026-08-08 09:50:47 +0530

    Use MDA theme colors

--- mdadash/frontend/src/plugins/vuetify.js
+++ mdadash/frontend/src/plugins/vuetify.js
@@ -13,5 +13,20 @@ export default createVuetify({
   },
   theme: {
     defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#343131', // dark gray
+          'on-primary': '#FFFFFF', // white
+          secondary: '#808080', // gray
+          'on-secondary': '#FFFFFF', // white
+          background: '#E6E6E6', // light gray
+          'on-background': '#343131', // dark gray
+          surface: '#FFFFFF', // white
+          'on-surface': '#343131', // dark gray
+          info: '#FF9200', // orange
+        },
+      },
+    },
   },
 })
